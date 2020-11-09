202820d13170837d2ec8f34d2512837dbf3bc0a2
Phyo Thiha Kyaw <mr.phyothihakyaw9@gmail.com>
2020-11-09 11:01:06 +0630

    Add project files.

---
+++ src/SMS.Data/Models/SMSDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SMS.Data.Models
+{
+    public class SMSDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public SMSDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+        }
+
+        public static SMSDbContext Create()
+        {
+            return new SMSDbContext();
+        }
+    }
+}
