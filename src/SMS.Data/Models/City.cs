96cb1d6cbdd8336f13fd8e54a4d64744b3d4cca0
Phyo Thiha Kyaw <mr.phyothihakyaw9@gmail.com>
2020-11-10 12:55:28 +0630

    Moved system default types to Common

--- src/SMS.Data/Models/City.cs
+++ src/SMS.Data/Models/City.cs
@@ -1,16 +1,10 @@
-﻿using System;
-
-namespace SMS.Data.Models
+﻿namespace SMS.Data.Models
 {
-    public class City
+    public class City : Common
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsDelete { get; set; }
         public long Version { get; set; }
-        public string CreatedUserId { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public string UpdatedUserId { get; set; }
-        public DateTime UpdatedDate { get; set; }
     }
 }
---
+++ src/SMS.Data/Models/Common.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SMS.Data.Models
+{
+    public class Common
+    {
+        public string CreatedUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedUserId { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
